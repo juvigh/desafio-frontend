343b0468ce58b68d3858a3ebc48f442f5d032374
Julio <juliolk.4321@gmail.com>
2023-05-04 20:47:23 -0300

    Reestruturando arquivo colors para utilização das variáveis constantes

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,5 +1,17 @@
-const CTAMedium = "#29D6E7"
-const PrimaryXdark = "#031F3D"
-const PrimaryDark = "#0C4888"
-const AcessórioUm = "#0C488880"
-const AcessórioDois = "#28ADBA"
+const colors = {
+  CTAMedium: "#29D6E7",
+  PrimaryXdark: "#031F3D",
+  PrimaryDark: "#0C4888",
+  AcessorioUm: "#0C488880",
+  AcessorioDois: "#28ADBA",
+  NeutralWhite: "#FFFFFF",
+  NeutralXlight: "#FDFDFD",
+  NeutralLight: "#C5C5C5",
+  NeutralMedium: "#908E8E",
+  NeutralDark: "#3A3335",
+  NeutralXdark: "#484848",
+  FeedbackSuccess: "#2AC084",
+  FeedbackError: "#EB5E55",
+};
+
+export default colors
