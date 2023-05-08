a5bd5a11c7ad509449500b989ffcbc7b58b038e1
Julio <juliolk.4321@gmail.com>
2023-05-08 20:43:11 -0300

    Add H4 component with typography and color styles from typography and colors files

---
+++ src/styles/typography/Headings/H4.tsx
@@ -0,0 +1,18 @@
+import { colors } from "../../colors";
+import { typography } from "../typography";
+
+export const H4 = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <h4
+            style={{
+                fontFamily: typography.font.family.primary,
+                fontSize: typography.font.size.large,
+                fontWeight: typography.font.weight.extraBold,
+                color: colors.PrimaryDark,
+                lineHeight: typography.font.lineHeight.medium,
+            }}
+        >
+            {children}
+        </h4>
+    );
+};
