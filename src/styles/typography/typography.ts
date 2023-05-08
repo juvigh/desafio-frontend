c579b530822b9425faf4c23abaa86aee9d4e02a6
Julio <juliolk.4321@gmail.com>
2023-05-08 20:31:33 -0300

    Add 'font' object to 'typography.ts' file with the characteristics of the font used in the project.

---
+++ src/styles/typography/typography.ts
@@ -0,0 +1,28 @@
+export const typography = {
+  font: {
+    family: {
+      primary: "Poppins",
+    },
+    lineHeight: {
+      small: 40 + "px",
+      medium: 48 + "px",
+      large: 60 + "px",
+    },
+    size: {
+      xxxxLarge: 40 + "px",
+      xxxLarge: 36 + "px",
+      xxLarge: 28 + "px",
+      xLarge: 18 + "px",
+      large: 16 + "px",
+      medium: 14 + "px",
+      small: 12 + "px",
+    },
+    weight: {
+      extraBold: 800,
+      bold: 700,
+      semiBold: 600,
+      medium: 500,
+      regular: 400,
+    },
+  },
+};
