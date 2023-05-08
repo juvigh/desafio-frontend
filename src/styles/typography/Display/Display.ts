23d308bcf0c8f1a92243049b67c993b50d0c0f3a
Julio <juliolk.4321@gmail.com>
2023-05-08 20:40:35 -0300

    Add DisplayStyle object to use in the project

---
+++ src/styles/typography/Display/Display.ts
@@ -0,0 +1,9 @@
+import { colors } from "../../colors";
+import { typography } from "../typography";
+
+export const DisplayStyle = {
+  family: typography.font.family.primary,
+  size: typography.font.size.xxxxLarge,
+  weight: typography.font.weight.bold,
+  color: colors.PrimaryDark,
+};
