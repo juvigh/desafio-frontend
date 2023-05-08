6a3899a7c10f26e75f92a345047fe82326c048de
Julio <juliolk.4321@gmail.com>
2023-05-08 20:53:26 -0300

    Add LL and ML components with font weight and color properties in typography file imports

---
+++ src/styles/typography/StyleCustoms/Label.tsx
@@ -0,0 +1,46 @@
+import { colors } from "../../colors";
+import { typography } from "../typography";
+
+type LLProps = {
+  children: React.ReactNode;
+  weight: "regular" | "bold" | "semiBold";
+};
+
+export const LL = ({ children, weight }: LLProps) => {
+  let fontWeight
+  if (weight === "regular") {
+    fontWeight = typography.font.weight.regular
+  } else if (weight === "bold") {
+    fontWeight = typography.font.weight.bold
+  } else {
+    fontWeight = typography.font.weight.semiBold;
+  }
+  return (
+    <label
+      style={{
+        fontSize: typography.font.size.large,
+        fontWeight: fontWeight,
+        color: colors.PrimaryXdark,
+      }}
+    >
+      {children}
+    </label>
+  );
+};
+
+export const ML = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <label
+      style={{
+        fontSize: typography.font.size.medium,
+        fontWeight: typography.font.weight.regular,
+        color: colors.NeutralWhite,
+      }}
+    >
+      {children}
+    </label>
+  );
+};
+
+// LL = Large label
+// ML = Medium label
