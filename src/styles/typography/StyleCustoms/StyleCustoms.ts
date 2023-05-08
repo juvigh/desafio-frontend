612e252a50eb8320f8ee6f3108df6658035fc0f6
Julio <juliolk.4321@gmail.com>
2023-05-08 20:55:21 -0300

    Add customStyles object with caption and prices properties handling in typography file imports

---
+++ src/styles/typography/StyleCustoms/StyleCustoms.ts
@@ -0,0 +1,27 @@
+import { colors } from "../../colors";
+import { typography } from "../typography";
+
+export const customStyles = {
+  caption: {
+    size: typography.font.size.small,
+    weight: typography.font.weight.regular,
+    color: colors.NeutralXdark,
+  },
+  prices: {
+    bigPrice: {
+      size: typography.font.size.xxxLarge,
+      weight: typography.font.weight.bold,
+      color: colors.NeutralXdark,
+    },
+    mediumPrice: {
+      size: typography.font.size.xLarge,
+      weight: typography.font.weight.bold,
+      color: colors.NeutralXdark,
+    },
+    smallPrice: {
+      size: typography.font.size.medium,
+      weight: typography.font.weight.regular,
+      color: colors.NeutralXdark,
+    },
+  },
+};
