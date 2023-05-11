1570d1593a43a22a2c05ab1b8c2fa4224a52b587
Julio <juliolk.4321@gmail.com>
2023-05-11 12:33:41 -0300

    Refactor DisplayStyle to Display component with updated styles

--- src/styles/typography/display.ts
+++
@@ -1,9 +0,0 @@
-import { colors } from "../colors";
-import { typography } from "./typography";
-
-export const DisplayStyle = {
-  family: typography.family.primary,
-  size: typography.size.xxxxLarge,
-  weight: typography.weight.bold,
-  color: colors.PrimaryDark,
-};
