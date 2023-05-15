baf8b54a341e7b5d1ea0da439af4ade76a9edb5f
Julio <juliolk.4321@gmail.com>
2023-05-14 22:54:34 -0300

    Update App component to demonstrate button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import { PrimaryButton } from './components/buttons/button-primary';
+import { SecondaryButton } from './components/buttons/button-secondary';
+import { CTAButton } from './components/buttons/button-cta';
 import './App.css';
+import { H1 } from './styles/typography/h1';
+import { H2 } from './styles/typography/h2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <H1> Styleguide </H1>
+      <H2> Botões </H2>
+      <PrimaryButton hasIcon={false} disable={false} size="default" onClick={() => alert('hello')}>
+        Botao primario
+      </PrimaryButton>
+      <br></br>
+      <PrimaryButton hasIcon={false} disable={false} size="compact">
+        Botao primario
+      </PrimaryButton>
+      <br></br>
+      <PrimaryButton hasIcon={false} disable={true} size="default">
+        Botao primario
+      </PrimaryButton>
+      <br></br>
+      <PrimaryButton hasIcon={false} disable={true} size="compact">
+        Botao primario
+      </PrimaryButton>
+      <br></br>
+      <SecondaryButton hasIcon={false} size="default">
+        Botao secundario
+      </SecondaryButton>
+      <br></br>
+      <SecondaryButton hasIcon={false} size="compact">
+        Botao secundario
+      </SecondaryButton>
+      <br></br>
+      <CTAButton hasIcon={false} size="default">
+        Botao CTA
+      </CTAButton>
+      <br></br>
+      <CTAButton hasIcon={false} size="compact">
+        Botao CTA
+      </CTAButton>
+      <br></br>
     </div>
   );
 }
