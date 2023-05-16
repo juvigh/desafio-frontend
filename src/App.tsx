1783d6fcd169334caeaa70c09c5590bbfb97b45b
Julio <juliolk.4321@gmail.com>
2023-05-16 10:33:04 -0300

    Update App component and styles to display updated buttons and add spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,44 @@
 import React from 'react';
-import { PrimaryButton } from './components/buttons/button-primary';
-import { SecondaryButton } from './components/buttons/button-secondary';
-import { CTAButton } from './components/buttons/button-cta';
 import './App.css';
 import { H1 } from './styles/typography/h1';
 import { H2 } from './styles/typography/h2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Button } from './components/buttons/button';
 
 function App() {
   return (
     <div className="App">
       <H1> Styleguide </H1>
       <H2> Botões </H2>
-      <PrimaryButton hasIcon={false} disable={false} size="default" onClick={() => alert('hello')}>
-        Botao primario
-      </PrimaryButton>
-      <br></br>
-      <PrimaryButton hasIcon={false} disable={false} size="compact">
-        Botao primario
-      </PrimaryButton>
-      <br></br>
-      <PrimaryButton hasIcon={false} disable={true} size="default">
-        Botao primario
-      </PrimaryButton>
-      <br></br>
-      <PrimaryButton hasIcon={false} disable={true} size="compact">
-        Botao primario
-      </PrimaryButton>
-      <br></br>
-      <SecondaryButton hasIcon={false} size="default">
-        Botao secundario
-      </SecondaryButton>
-      <br></br>
-      <SecondaryButton hasIcon={false} size="compact">
-        Botao secundario
-      </SecondaryButton>
-      <br></br>
-      <CTAButton hasIcon={false} size="default">
-        Botao CTA
-      </CTAButton>
-      <br></br>
-      <CTAButton hasIcon={false} size="compact">
-        Botao CTA
-      </CTAButton>
-      <br></br>
+      <Button type="primary" icon={<FontAwesomeIcon icon={faHeart} />}>
+        primary button
+      </Button>
+      <div className={'spacing'}></div>
+      <Button type="secondary">secondary button</Button>
+      <div className={'spacing'}></div>
+      <Button type="cta">Ctabutton</Button>
+      <div className={'spacing'}></div>
+      <Button type="primary" icon={<FontAwesomeIcon icon={faHeart} />} compact>
+        primary button
+      </Button>
+      <div className={'spacing'}></div>
+      <Button type="secondary" compact>
+        secondary button
+      </Button>
+      <div className={'spacing'}></div>
+      <Button type="cta" compact>
+        Ctabutton
+      </Button>
+      <div className={'spacing'}></div>
+      <Button type="primary" disable>
+        primary button
+      </Button>
+      <div className={'spacing'}></div>
+      <Button type="primary" disable compact>
+        primary button
+      </Button>
+      <div className={'spacing'}></div>
     </div>
   );
 }
