1c0c755f90e3fd4ef5ae2c7d6df847f3d648e220
Julio <juliolk.4321@gmail.com>
2023-05-22 10:05:40 -0300

    Update App component to test Form components

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,27 @@
 import React from 'react';
 import './App.css';
 import { H1 } from './styles/typography/h1';
-import { H2 } from './styles/typography/h2';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Form } from './components/forms/form';
 import { Separator } from './components/separator/separator';
 import { Button } from './components/buttons/button';
+import PropertyInfo from './components/property-info/propertyInfo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+// import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
       <H1> Styleguide </H1>
-      <H2> Botões </H2>
-      <Button type="primary" icon={<FontAwesomeIcon icon={faHeart} />}>
-        primary button
-      </Button>
-      <div className={'spacing'}></div>
-      <Button type="secondary">secondary button</Button>
-      <div className={'spacing'}></div>
-      <Button type="cta">Ctabutton</Button>
-      <div className={'spacing'}></div>
-      <Button type="primary" icon={<FontAwesomeIcon icon={faHeart} />} compact>
-        primary button
-      </Button>
-      <div className={'spacing'}></div>
-      <Button type="secondary" compact>
-        secondary button
-      </Button>
-      <div className={'spacing'}></div>
-      <Button type="cta" compact>
-        Ctabutton
-      </Button>
-      <div className={'spacing'}></div>
-      <Button type="primary" disable>
-        primary button
-      </Button>
-      <div className={'spacing'}></div>
-      <Button type="primary" disable compact>
-        primary button
-      </Button>
-      <div className={'spacing'}></div>
+      <Form title="Nome"></Form>
       <Separator className="spacing"></Separator>
+      <Form title="Email"></Form>
       <Separator className="spacing"></Separator>
+      <Form title="Senha"></Form>
       <Separator className="spacing"></Separator>
+      <Button type="primary">Cadastrar</Button>
       <Separator className="spacing"></Separator>
+      <PropertyInfo title="Carro" icon={<FontAwesomeIcon icon={faCar} />}></PropertyInfo>
       <Separator className="spacing"></Separator>
     </div>
   );
