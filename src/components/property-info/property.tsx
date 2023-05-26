c7282b37acf70be49771f461f1b78118cfedb875
Julio <juliolk.4321@gmail.com>
2023-05-26 14:24:25 -0300

    Add CSS classes and update styles to PropertyInfo component

--- src/components/property-info/property.tsx
+++ src/components/property-info/property.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { propertyStyles } from './property-info';
+import { iconStyle, propertyStyles } from './property-info';
 import { H4 } from '../../styles/typography/h4';
 import { colors } from '../../styles/colors';
+import './property-info.css';
 
 interface PropertyInfoProps {
   icon: React.ReactNode;
   title: string;
-  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
 }
 
-export const PropertyInfo = ({ icon, title, flexDirection }: PropertyInfoProps) => {
-  const propertyStyle = {
-    ...propertyStyles,
-    flexDirection: flexDirection || 'column',
-  };
+export const PropertyInfo = ({ icon, title }: PropertyInfoProps) => {
   return (
-    <div style={propertyStyle}>
-      <div style={propertyStyle.icon}>{icon}</div>
+    <div className={'propertyConteiner'} style={propertyStyles}>
+      <div style={iconStyle}>{icon}</div>
       <H4 style={{ color: colors.NeutralXdark, lineHeight: undefined }}>{title}</H4>
     </div>
   );
