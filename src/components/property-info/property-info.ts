603f7799855ae84b2e53a45bd14358b21574642d
Julio <juliolk.4321@gmail.com>
2023-05-26 14:21:23 -0300

    Refactor propertyStyles and add iconStyle

--- src/components/property-info/property-info.ts
+++ src/components/property-info/property-info.ts
@@ -2,18 +2,14 @@ import { colors } from '../../styles/colors';
 import { typography } from '../../styles/typography/typography';
 
 export const propertyStyles = {
-  display: 'flex',
   backgroundColor: colors.NeutralXlight,
+  border: `1px solid ${colors.NeutralXlight}`,
+  borderRadius: typography.border.radiusLarge,
+  padding: `2px ${typography.size.xxxxLarge}`,
+};
+
+export const iconStyle = {
+  padding: typography.size.small,
   color: colors.NeutralXdark,
   fontSize: typography.size.medium,
-  border: `1px solid ${colors.NeutralXlight}`,
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderRadius: typography.border.radiusLarge,
-  width: '100px',
-  height: '95px',
-  padding: typography.size.small,
-  icon: {
-    padding: typography.size.small,
-  },
 };
