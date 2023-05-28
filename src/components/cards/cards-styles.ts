aef6adb4b5a3ea8dcba53df499701976f9d9aed8
Julio <juliolk.4321@gmail.com>
2023-05-28 20:29:00 -0300

    Add styles for CardCategory component

---
+++ src/components/cards/cards-styles.ts
@@ -0,0 +1,9 @@
+import { colors } from '../../styles/colors';
+import { typography } from '../../styles/typography/typography';
+
+export const cardCategoryStyles = {
+  display: 'flex',
+  maxWidth: '212px',
+  backgroundColor: colors.NeutralWhite,
+  borderRadius: typography.border.radius,
+};
