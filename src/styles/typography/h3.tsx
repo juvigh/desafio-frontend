c9a68f2f8a63f6a71ccf421dc349fa13a9f2a3de
Julio <juliolk.4321@gmail.com>
2023-05-28 20:17:39 -0300

    Add interface to H3 component

--- src/styles/typography/h3.tsx
+++ src/styles/typography/h3.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { colors } from '../colors';
 import { typography } from './typography';
+interface H3Props {
+  children: React.ReactNode;
+  weight?: keyof typeof typography.weight;
+  color?: keyof typeof colors;
+  lineHeight?: string;
+}
 
-export const H3 = ({ children }: { children: React.ReactNode }) => {
+export const H3 = ({ children, color, weight, lineHeight }: H3Props) => {
   return (
     <h3
       style={{
