ce191b1f8e9740cabab1d5267f74cc544db24037
Julio <juliolk.4321@gmail.com>
2023-06-01 12:37:04 -0300

    Fix style file

--- src/components/cards/cards-styles.ts
+++ src/components/cards/cards-styles.ts
@@ -1,30 +1,7 @@
 import { colors } from '../../styles/colors';
-import { customStyles } from '../../styles/typography/style-customs';
 import { typography } from '../../styles/typography/typography';
 
-const commonStyles = {
+export const commonStyles = {
   backgroundColor: colors.NeutralWhite,
   borderRadius: typography.border.radius,
-  caption: {
-    fontSize: customStyles.caption.size,
-    color: customStyles.caption.color,
-    fontWeight: customStyles.caption.weight,
-  },
-  mediumPrice: {
-    fontSize: customStyles.prices.mediumPrice.size,
-    color: customStyles.prices.mediumPrice.color,
-    fontWeight: customStyles.prices.mediumPrice.weight,
-  },
-};
-
-export const cardCategoryStyles = {
-  ...commonStyles,
-};
-
-export const cardImovelStyles = {
-  ...commonStyles,
-};
-
-export const facilitiesImovelStyle = {
-  ...commonStyles,
 };
