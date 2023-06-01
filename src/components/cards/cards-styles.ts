a407936faac2cb3b061141947fb83ef7cdc0d08d
Julio <juliolk.4321@gmail.com>
2023-06-01 11:04:42 -0300

    Changed style of CardCategory component

--- src/components/cards/cards-styles.ts
+++ src/components/cards/cards-styles.ts
@@ -1,9 +1,8 @@
+import { customStyles } from './../../styles/typography/style-customs';
 import { colors } from '../../styles/colors';
 import { typography } from '../../styles/typography/typography';
 
 export const cardCategoryStyles = {
-  display: 'flex',
-  maxWidth: '212px',
   backgroundColor: colors.NeutralWhite,
   borderRadius: typography.border.radius,
 };
