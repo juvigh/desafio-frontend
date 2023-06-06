f1d2f53bbb3abe6fd1d895a2101cd96ab97a1c49
Julio <juliolk.4321@gmail.com>
2023-06-06 10:15:54 -0300

    Add color prop to Caption component for customizable text color

--- src/styles/typography/caption.tsx
+++ src/styles/typography/caption.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { typography } from './typography';
 import { colors } from '../colors';
 
-export const Caption = ({ children }: { children: React.ReactNode }) => {
+interface CaptionProps {
+  color?: keyof typeof colors;
+  children: React.ReactNode;
+}
+
+export const Caption = ({ children, color }: CaptionProps) => {
   return (
     <p
       style={{
         fontFamily: typography.family.primary,
         fontSize: typography.size.small,
         fontWeight: typography.weight.regular,
-        color: colors.NeutralXdark,
+        color: color ? colors[color] : colors.NeutralXdark,
       }}
     >
       {children}
