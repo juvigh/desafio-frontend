b88b3ffa802b826d37d19754264f03bb573291e2
Julio <juliolk.4321@gmail.com>
2023-06-06 10:54:55 -0300

    Created Divider component and its styles

---
+++ src/components/divider/divider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './divider.css';
+
+const Divider = () => {
+  return (
+    <div className="container-divider">
+      <hr className="divider" />
+    </div>
+  );
+};
+
+export default Divider;
