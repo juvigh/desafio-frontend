afe4bfdac1d95c05c3539df40219f5d24f9187f8
Julio <juliolk.4321@gmail.com>
2023-06-14 09:44:12 -0300

    Added heart icon functionality

--- src/components/cards/card-property.tsx
+++ src/components/cards/card-property.tsx
@@ -3,9 +3,11 @@ import { Body2 } from '../../styles/typography/body-2';
 import './cards.css';
 import { FacilitiesProperty } from './facilities-property';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
 import { Price } from '../../styles/typography/prices';
 import { Caption } from '../../styles/typography/caption';
+import { useState } from 'react';
 
 interface CardPropertyProps {
   image: string;
@@ -18,6 +20,12 @@ interface CardPropertyProps {
 }
 
 export const CardProperty = ({ image, title, description, price, beds, bath, dimension }: CardPropertyProps) => {
+  const [isHearted, setIsHearted] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsHearted(!isHearted);
+  };
+
   const formatPrice = (price: number) => {
     const formattedPrice = (price / 100).toFixed(2);
     return `R$ ${formattedPrice}`;
@@ -30,8 +38,12 @@ export const CardProperty = ({ image, title, description, price, beds, bath, dim
       </div>
 
       <div className={'price-container'}>
-        <FontAwesomeIcon icon={faHeart} />
         <Price size="medium">{formatPrice(price)}</Price>
+        <FontAwesomeIcon
+          icon={isHearted ? faHeartSolid : faHeartRegular}
+          className={isHearted ? 'hearted' : ''}
+          onClick={handleHeartClick}
+        />
       </div>
       <div className={'description-container'}>
         <Body2 weight="bold"> {title}</Body2>
