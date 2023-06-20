2bbf792f84bedea0622010718b5973c38c6cdb92
Julio <juliolk.4321@gmail.com>
2023-06-20 11:46:18 -0300

    Added 'color' prop to H2 component

--- src/styles/typography/h2.tsx
+++ src/styles/typography/h2.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { colors } from '../colors';
 import { typography } from './typography';
 
-export const H2 = ({ children }: { children: React.ReactNode }) => {
+interface H2Props {
+  children: React.ReactNode;
+  color?: keyof typeof colors;
+}
+
+export const H2 = ({ children, color }: H2Props) => {
   return (
     <h2
       style={{
         fontFamily: typography.family.primary,
         fontSize: typography.size.xxLarge,
         fontWeight: typography.weight.regular,
-        color: colors.PrimaryDark,
+        color: color ? colors[color] : colors.PrimaryDark,
         lineHeight: typography.lineHeight.medium,
       }}
     >
