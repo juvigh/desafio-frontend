5bd6f0a34345d7928b4bb62cb8a4b731cbdcfd3b
Julio <juliolk.4321@gmail.com>
2023-06-21 17:51:17 -0300

    Updated app.tsx and its styles for testing CardCarouselSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,151 @@
 import React from 'react';
 import './App.css';
-import { H1 } from './styles/typography/h1';
-import { FormField } from './components/forms/form-field';
 import { Separator } from './components/separator/separator';
-import { Button } from './components/buttons/button';
-import { faCar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCar } from '@fortawesome/free-solid-svg-icons';
-import { PropertyInfo } from './components/property-info/property';
-import PropertyInfo from './components/property-info/property';
+import { CardCarouselSection } from './components/carousel/carousel-section/card-carousel-section';
+import { CardData } from './components/carousel/carousel-card';
+
+const categoryCardsData: CardData[] = [
+  {
+    type: 'category',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão Azul',
+      numberOfProperties: 70,
+    },
+  },
+  {
+    type: 'category',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão Azul',
+      numberOfProperties: 70,
+    },
+  },
+  {
+    type: 'category',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão Azul',
+      numberOfProperties: 70,
+    },
+  },
+  {
+    type: 'category',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão Azul',
+      numberOfProperties: 70,
+    },
+  },
+  {
+    type: 'category',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão Azul',
+      numberOfProperties: 70,
+    },
+  },
+  {
+    type: 'category',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão Azul',
+      numberOfProperties: 70,
+    },
+  },
+  {
+    type: 'category',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão Azul',
+      numberOfProperties: 70,
+    },
+  },
+];
+
+const propertyCardsData: CardData[] = [
+  {
+    type: 'property',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão azul',
+      description: 'Alphaville, SP',
+      price: 100000,
+      beds: 2,
+      bath: 1,
+      dimension: '120',
+    },
+  },
+  {
+    type: 'property',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão azul',
+      description: 'Alphaville, SP',
+      price: 100000,
+      beds: 2,
+      bath: 1,
+      dimension: '120',
+    },
+  },
+  {
+    type: 'property',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão azul',
+      description: 'Alphaville, SP',
+      price: 100000,
+      beds: 2,
+      bath: 1,
+      dimension: '120',
+    },
+  },
+  {
+    type: 'property',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão azul',
+      description: 'Alphaville, SP',
+      price: 100000,
+      beds: 2,
+      bath: 1,
+      dimension: '120',
+    },
+  },
+  {
+    type: 'property',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão azul',
+      description: 'Alphaville, SP',
+      price: 100000,
+      beds: 2,
+      bath: 1,
+      dimension: '120',
+    },
+  },
+  {
+    type: 'property',
+    data: {
+      image: 'https://images.adsttc.com/media/images/6152/07d0/e5d7/3801/659d/8d4d/medium_jpg/3.jpg?1632765994',
+      title: 'Mansão azul',
+      description: 'Alphaville, SP',
+      price: 100000,
+      beds: 2,
+      bath: 1,
+      dimension: '120',
+    },
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <H1> Styleguide </H1>
-      <Button type="primary">Cadastrar</Button>
-      <PropertyInfo title="Carro" icon={<FontAwesomeIcon icon={faCar} />}></PropertyInfo>
-      <FormField title="Nome"></FormField>
       <Separator vertical size={20} />
-      <FormField title="Email"></FormField>
+      <CardCarouselSection title="Encontre o imóvel ideal para o seu estilo de vida!" cardsData={categoryCardsData} />
       <Separator vertical size={20} />
-      <FormField title="Senha"></FormField>
+      <CardCarouselSection title="Lançamentos que você pode se interessar!" cardsData={propertyCardsData} />
       <Separator vertical size={20} />
-      <Separator vertical size={20} />
-      <PropertyInfo title="Carro" icon={<FontAwesomeIcon icon={faCar} />}></PropertyInfo>
     </div>
   );
 }
