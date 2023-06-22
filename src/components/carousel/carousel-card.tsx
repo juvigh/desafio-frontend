49e2b04911330d92015fff11611832206e940319
Julio <juliolk.4321@gmail.com>
2023-06-22 15:57:01 -0300

    Refactor CarouselCard component to use children prop instead of cardsData

--- src/components/carousel/carousel-card.tsx
+++ src/components/carousel/carousel-card.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import { CardCategory, CardCategoryProps } from '../cards/card-category';
-import { CardProperty, CardPropertyProps } from '../cards/card-property';
 import './carousel.css';
 
-export interface CardData {
-  type: 'property' | 'category';
-  data: CardPropertyProps | CardCategoryProps;
-}
-
-interface CarouselCardProps {
-  cardsData: CardData[];
-}
-
-export const CarouselCard = ({ cardsData }: CarouselCardProps) => {
+export const CarouselCard = ({ children }: { children: React.ReactNode }) => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -33,20 +22,11 @@ export const CarouselCard = ({ cardsData }: CarouselCardProps) => {
     },
   };
 
-  const renderCard = (cardData: CardData) => {
-    if (cardData.type === 'property') {
-      return <CardProperty {...(cardData.data as CardPropertyProps)} />;
-    } else if (cardData.type === 'category') {
-      return <CardCategory {...(cardData.data as CardCategoryProps)} />;
-    }
     return null;
   };
 
   return (
     <Carousel responsive={responsive} className="carousel-card">
-      {cardsData.map((card, index) => {
-        return <div key={index}>{renderCard(card)}</div>;
-      })}
     </Carousel>
   );
 };
