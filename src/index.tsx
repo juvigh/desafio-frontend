dbb200e67473f0711dbe56ce0a32395d91f840d1
Julio <juliolk.4321@gmail.com>
2023-07-03 16:54:12 -0300

    Add Apollo Provider for GraphQL integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ApolloProvider } from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { client } from './api/graphql';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
