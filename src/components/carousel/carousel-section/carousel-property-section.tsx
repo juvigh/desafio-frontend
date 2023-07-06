74748ac357dec87b5dfbe2eb367a359f513cbb0f
Julio <juliolk.4321@gmail.com>
2023-07-06 11:39:50 -0300

    Added PropertyCarousel component

---
+++ src/components/carousel/carousel-section/carousel-property-section.tsx
@@ -0,0 +1,36 @@
+import { fetchProperties } from '../../../api/fetch-properties';
+import { CardProperty } from '../../cards/card-property';
+import { LoadingError } from '../../loading/loading-error';
+import { CardCarouselSection } from './card-carousel-section';
+
+export const PropertyCarousel = () => {
+  const { data = [], loading, error } = fetchProperties();
+  const property = data[0];
+
+  return (
+    <>
+      <LoadingError
+        loading={loading}
+        error={error}
+        emptyMessage="Não há lançamentos disponíveis."
+        dataLength={data.length}
+      />
+      {!loading && !error && data.length > 0 && (
+        <CardCarouselSection title="Lançamentos que você pode se interessar!">
+          <CardProperty
+            key={property.id}
+            price={property.buyPrices.total}
+            image={property.imageUrls}
+            streetNumber={property.address.streetNumber}
+            beds={property.bedrooms}
+            bath={property.bathrooms}
+            dimension={property.area}
+            district={property.address.district}
+            state={property.address.state}
+            city={property.address.city}
+          />
+        </CardCarouselSection>
+      )}
+    </>
+  );
+};
