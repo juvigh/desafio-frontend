9a16da0079060e57651136b4c8c938c4a6c11dd6
Julio <juliolk.4321@gmail.com>
2023-07-07 16:16:28 -0300

    Add "about-us" route and component

---
+++ src/pages/about-us/about-us.tsx
@@ -0,0 +1,13 @@
+import { Menu } from '../../components/menu/menu';
+import { Separator } from '../../components/separator/separator';
+import { H1 } from '../../styles/typography/h1';
+
+export const AboutUs = () => {
+  return (
+    <div>
+      <Menu />
+      <Separator vertical size={20} />
+      <H1> Em construção </H1>
+    </div>
+  );
+};
