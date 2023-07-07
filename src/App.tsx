3a062b1d11772bc1997e485d383eb9e8e339e8bd
Julio <juliolk.4321@gmail.com>
2023-07-07 16:16:28 -0300

    Add "about-us" route and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
+import { AboutUs } from './pages/about-us/about-us';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </Router>
   );
