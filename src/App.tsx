ff0e2ecba4b41fc4eef0d4a8da3d12c583acab34
Julio <juliolk.4321@gmail.com>
2023-07-07 16:16:46 -0300

    Add "contact" route and component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
 import { AboutUs } from './pages/about-us/about-us';
+import { Contact } from './pages/contact/contact';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
