9614b11c273e00552248669d6592e3366dbff2e1
Julio <juliolk.4321@gmail.com>
2023-07-10 19:33:46 -0300

    Update carousel-property-section.tsx

--- src/components/carousel/carousel-section/carousel-property-section.tsx
+++ src/components/carousel/carousel-section/carousel-property-section.tsx
@@ -1,30 +1,30 @@
 import { fetchProperties } from '../../../api/fetch-properties';
+import { Property } from '../../../api/queries/get-property';
 import { CardProperty } from '../../cards/card-property';
 import { LoadingError } from '../../loading/loading-error';
 import { CardCarouselSection } from './card-carousel-section';
 
 export const PropertyCarousel = () => {
   const { data = [], loading, error } = fetchProperties();
-  const property = data[0];
 
   return (
-    <>
-      <LoadingError loading={loading} error={!!error} emptyMessage="Não há lançamentos disponíveis." data={!!data}>
-        <CardCarouselSection title="Lançamentos que você pode se interessar!">
+    <LoadingError loading={loading} error={!!error} emptyMessage="Não há lançamentos disponíveis." data={!!data}>
+      <CardCarouselSection title="Lançamentos que você pode se interessar!">
+        {data.map((property: Property) => (
           <CardProperty
-            key={property.id}
-            price={property.buyPrices.total}
-            image={property.imageUrls}
-            streetNumber={property.address.streetNumber}
-            beds={property.bedrooms}
-            bath={property.bathrooms}
-            dimension={property.area}
-            district={property.address.district}
-            state={property.address.state}
-            city={property.address.city}
+            key={property?.id}
+            price={property?.buyPrices?.total}
+            image={property?.imageUrls}
+            streetNumber={property?.address?.streetNumber}
+            beds={property?.bedrooms}
+            bath={property?.bathrooms}
+            dimension={property?.area}
+            district={property?.address?.district}
+            state={property?.address?.state}
+            city={property?.address?.city}
           />
-        </CardCarouselSection>
-      </LoadingError>
-    </>
+        ))}
+      </CardCarouselSection>
+    </LoadingError>
   );
 };
