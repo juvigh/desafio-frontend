7f21403ca1c284f9dedcedb61ae1f9beb5dcf4eb
Julio <juliolk.4321@gmail.com>
2023-07-10 19:33:30 -0300

    Format price to use locale-specific formatting

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,4 +1,7 @@
 export const formatPrice = (price: number) => {
-  const formattedPrice = (price / 100).toFixed(0);
-  return `R$ ${formattedPrice}`;
+  const formattedPrice = (price / 100).toLocaleString('pt-BR', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+  return `R$${formattedPrice}`;
 };
