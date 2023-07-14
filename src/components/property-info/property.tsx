4a0f98890f44442a3eef564b46c6a0f37f814973
Julio <juliolk.4321@gmail.com>
2023-07-14 12:43:26 -0300

    Refactor H4 component to accept color prop and weight prop

--- src/components/property-info/property.tsx
+++ src/components/property-info/property.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { iconStyle, propertyStyles } from './property-info';
 import { H4 } from '../../styles/typography/h4';
-import { colors } from '../../styles/colors';
 import './property-info.css';
 
 interface PropertyInfoProps {
@@ -13,7 +12,7 @@ export const PropertyInfo = ({ icon, title }: PropertyInfoProps) => {
   return (
     <div className={'propertyConteiner'} style={propertyStyles}>
       <div style={iconStyle}>{icon}</div>
-      <H4 style={{ color: colors.NeutralXdark, lineHeight: undefined }}>{title}</H4>
+      <H4 weight="semiBold" color="NeutralXdark">
     </div>
   );
 };
