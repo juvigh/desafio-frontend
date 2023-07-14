b144ea7c94826577992a9c09c44fd477c87d2fbe
Julio <juliolk.4321@gmail.com>
2023-07-14 12:46:37 -0300

    Add separator and number prop to PropertyInfo component

--- src/components/property-info/property.tsx
+++ src/components/property-info/property.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { iconStyle, propertyStyles } from './property-info';
 import { H4 } from '../../styles/typography/h4';
 import './property-info.css';
+import { Separator } from '../separator/separator';
 
 interface PropertyInfoProps {
   icon: React.ReactNode;
   title: string;
+  number?: number;
 }
 
-export const PropertyInfo = ({ icon, title }: PropertyInfoProps) => {
+export const PropertyInfo = ({ icon, title, number }: PropertyInfoProps) => {
   return (
     <div className={'propertyConteiner'} style={propertyStyles}>
       <div style={iconStyle}>{icon}</div>
+      <Separator vertical size={20} />
       <H4 weight="semiBold" color="NeutralXdark">
+        {number} {title}
+      </H4>
     </div>
   );
 };
