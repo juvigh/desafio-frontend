d075017fb71333a9d42b6b3ef336be543df5b248
Julio <juliolk.4321@gmail.com>
2023-07-14 12:45:08 -0300

    Refactor H1 component to accept color prop

--- src/styles/typography/h1.tsx
+++ src/styles/typography/h1.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { colors } from '../colors';
 import { typography } from './typography';
 
-export const H1 = ({ children }: { children: React.ReactNode }) => {
+interface H1Props {
+  children: React.ReactNode;
+  color?: keyof typeof colors;
+}
+
+export const H1 = ({ children, color }: H1Props) => {
   return (
     <h1
       style={{
         fontFamily: typography.family.primary,
         fontSize: typography.size.xxxxLarge,
         fontWeight: typography.weight.semiBold,
-        color: colors.PrimaryDark,
+        color: color ? colors[color] : colors.PrimaryDark,
         lineHeight: typography.lineHeight.large,
         margin: 0,
       }}
