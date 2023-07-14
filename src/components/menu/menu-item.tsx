1773098cf20ef630983ee86e2ccd2bb33f6270a1
Julio <juliolk.4321@gmail.com>
2023-07-14 13:56:09 -0300

    Add useLocation to highlight active menu item

--- src/components/menu/menu-item.tsx
+++ src/components/menu/menu-item.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import { LargeLabel } from '../../styles/typography/label';
 import { MenuItemStyles } from './menu-styles';
 import { colors } from '../../styles/colors';
-import { Link } from 'react-router-dom';
 import './menu.css';
 
 interface MenuItemProps {
@@ -11,24 +11,27 @@ interface MenuItemProps {
 }
 
 export const MenuItem = ({ title, path }: MenuItemProps) => {
+  const location = useLocation();
   const [isHovered, setIsHovered] = useState(false);
 
   const handleHover = (hovered: boolean) => {
     setIsHovered(hovered);
   };
 
+  const isActive = location.pathname === path;
+
   return (
     <div
       className="menu-item"
       style={{
         ...MenuItemStyles,
-        borderBottom: `5px solid ${isHovered ? colors.CTA : colors.NeutralWhite}`,
+        borderBottom: `5px solid ${isActive ? colors.CTA : isHovered ? colors.CTA : colors.NeutralWhite}`,
       }}
       onMouseEnter={() => handleHover(true)}
       onMouseLeave={() => handleHover(false)}
     >
       <Link to={path}>
-        <LargeLabel color="NeutralXdark" weight={isHovered ? 'bold' : 'semiBold'}>
+        <LargeLabel color="NeutralXdark" weight={isActive ? 'bold' : isHovered ? 'bold' : 'semiBold'}>
           {title}
         </LargeLabel>
       </Link>
