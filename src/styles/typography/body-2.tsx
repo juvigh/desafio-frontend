6cfbbca5f26190736c1d2b71699968df8bb2925c
Julio <juliolk.4321@gmail.com>
2023-07-18 17:28:06 -0300

    Added withLineThrough prop to the Body2 component

--- src/styles/typography/body-2.tsx
+++ src/styles/typography/body-2.tsx
@@ -6,9 +6,11 @@ interface Body2Props {
   children: React.ReactNode;
   weight: 'regular' | 'bold';
   color?: keyof typeof colors;
+  withLineThrough?: boolean;
 }
 
-export const Body2 = ({ children, weight, color }: Body2Props) => {
+export const Body2 = ({ children, weight, color, withLineThrough }: Body2Props) => {
+  const textDecoration = withLineThrough ? 'line-through' : 'none';
   return (
     <p
       style={{
@@ -17,6 +19,7 @@ export const Body2 = ({ children, weight, color }: Body2Props) => {
         fontWeight: typography.weight[weight === 'regular' ? 'regular' : 'bold'],
         color: color ? colors[color] : colors.NeutralXdark,
         margin: 0,
+        textDecoration,
       }}
     >
       {children}
