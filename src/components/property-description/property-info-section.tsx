50c2f3bf2b08eb1e92715ac20edc3f5a5237b493
Julio <juliolk.4321@gmail.com>
2023-07-19 18:01:57 -0300

    Updated the PropertyInfo component

--- src/components/property-description/property-info-section.tsx
+++ src/components/property-description/property-info-section.tsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PropertyInfo } from '../property-info/property';
 import './property-section.css';
 import { faBath, faBed, faCar, faRuler, faSwimmingPool } from '@fortawesome/free-solid-svg-icons';
@@ -14,11 +13,11 @@ interface PropertyInfoSectionProps {
 export const PropertyInfoSection = ({ bedroom, bathroom, pool, garage, area }: PropertyInfoSectionProps) => {
   return (
     <div className="property-info-container">
-      <PropertyInfo icon={<FontAwesomeIcon icon={faBed} />} title="Quartos" number={bedroom} />
-      <PropertyInfo icon={<FontAwesomeIcon icon={faBath} />} title="Banheiros" number={bathroom} />
-      <PropertyInfo icon={<FontAwesomeIcon icon={faRuler} />} title={area} />
-      {pool && <PropertyInfo icon={<FontAwesomeIcon icon={faSwimmingPool} />} title="Piscina" />}
-      {garage && <PropertyInfo icon={<FontAwesomeIcon icon={faCar} />} title="Vaga" />}
+      <PropertyInfo icon={faBed} title="Quartos" number={bedroom} />
+      <PropertyInfo icon={faBath} title="Banheiros" number={bathroom} />
+      <PropertyInfo icon={faRuler} title={area} />
+      {pool && <PropertyInfo icon={faSwimmingPool} title="Piscina" />}
+      {garage && <PropertyInfo icon={faCar} title="Vaga" />}
     </div>
   );
 };
--- src/components/property-info/property.tsx
+++ src/components/property-info/property.tsx
@@ -3,9 +3,11 @@ import { iconStyle, propertyStyles } from './property-info';
 import { H4 } from '../../styles/typography/h4';
 import './property-info.css';
 import { Separator } from '../separator/separator';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 interface PropertyInfoProps {
-  icon: React.ReactNode;
+  icon: IconDefinition;
   title: string;
   number?: number;
 }
@@ -13,7 +15,9 @@ interface PropertyInfoProps {
 export const PropertyInfo = ({ icon, title, number }: PropertyInfoProps) => {
   return (
     <div className={'propertyConteiner'} style={propertyStyles}>
-      <div style={iconStyle}>{icon}</div>
+      <div style={iconStyle}>
+        <FontAwesomeIcon icon={icon} />
+      </div>
       <Separator vertical size={20} />
       <H4 weight="semiBold" color="NeutralXdark">
         {number} {title}
