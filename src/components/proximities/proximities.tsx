467e96eb532870ef2f9668722ce19a2c25df0bf0
Julio <juliolk.4321@gmail.com>
2023-07-20 16:46:52 -0300

    Update handling of proximities, latitude, and longitude in the Proximities component

--- src/components/proximities/proximities.tsx
+++ src/components/proximities/proximities.tsx
@@ -4,9 +4,9 @@ import { ProximitiesSection } from './proximities-section';
 
 export const Proximities = () => {
   const { data = [], loading, error } = fetchProperties();
-  const proximities = data?.[0]?.proximities || [];
-  const latitude = data?.[0]?.address.latitude || 0;
-  const longitude = data?.[0]?.address.longitude || 0;
+  const proximities = data[0]?.proximities ?? [];
+  const latitude = data[0]?.address.latitude ?? 0;
+  const longitude = data[0]?.address.longitude ?? 0;
 
   return (
     <LoadingError loading={loading} error={!!error} emptyMessage="Não há proximidades" data={!!data}>
