59a19b32de4b4728913dd5014c092fd84a1a31bb
Julio <juliolk.4321@gmail.com>
2023-07-25 18:09:00 -0300

    Modify FormField component to accept additional props

--- src/components/forms/form-field.tsx
+++ src/components/forms/form-field.tsx
@@ -1,19 +1,35 @@
 import React, { useState, useMemo } from 'react';
 import { formFieldStyles } from './form-field-style';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
-import { Separator } from '../separator/separator';
 import './form-field.css';
+import { validateEmail } from '../../utils/regex';
+import { ErrorMessage } from './error-message';
 
 interface FormFieldStyles {
   flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
   title: string;
+  required?: boolean;
+  email?: boolean;
+  type?: string;
+  value: string;
+  onChange: (newValue: string) => void;
+  passwordMismatchError?: boolean;
+  isButtonClicked: boolean;
+  placeholder?: string;
 }
 
-export const FormField = ({ flexDirection, title }: FormFieldStyles) => {
+export const FormField = ({
+  flexDirection,
+  title,
+  required,
+  email,
+  type,
+  value,
+  onChange,
+  passwordMismatchError,
+  isButtonClicked,
+  placeholder,
+}: FormFieldStyles) => {
   const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
-  const [hasError, setHasError] = useState(false);
 
   const handleFocus = () => {
     setIsFocused(true);
@@ -24,20 +40,21 @@ export const FormField = ({ flexDirection, title }: FormFieldStyles) => {
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    setIsFilled(inputValue !== '');
-    setHasError(inputValue === '');
+    const newValue = e.target.value;
+    onChange(newValue);
   };
+
+  const hasError = (required && !value.trim() && isButtonClicked) || (email && value && !validateEmail(value));
 
   const formFieldStyle = useMemo(
     () => ({
       ...formFieldStyles,
       flexDirection: flexDirection || 'column',
       ...(isFocused && formFieldStyles.focused),
-      ...(isFilled && formFieldStyles.filled),
-      ...(hasError && formFieldStyles.error),
+      ...(value && formFieldStyles.filled),
+      ...(hasError || passwordMismatchError ? formFieldStyles.error : {}),
     }),
-    [formFieldStyles, flexDirection, isFocused, isFilled, hasError],
+    [formFieldStyles, flexDirection, isFocused, value, hasError, passwordMismatchError],
   );
 
   const inputStyles = useMemo(
@@ -50,18 +67,18 @@ export const FormField = ({ flexDirection, title }: FormFieldStyles) => {
   const labelStyles = useMemo(
     () => ({
       ...inputStyles,
-      ...(hasError && inputStyles.error),
+      ...(hasError || passwordMismatchError ? inputStyles.error : {}),
     }),
-    [inputStyles, hasError],
+    [inputStyles, hasError, passwordMismatchError],
   );
 
   const captionStyles = useMemo(
     () => ({
       ...inputStyles,
       ...inputStyles.error,
-      display: hasError ? 'flex' : 'none',
+      display: (isFocused || hasError || passwordMismatchError) && (required || email) ? 'flex' : 'none',
     }),
-    [inputStyles, hasError],
+    [inputStyles, hasError, required, email, passwordMismatchError],
   );
 
   return (
@@ -70,16 +87,18 @@ export const FormField = ({ flexDirection, title }: FormFieldStyles) => {
         <label style={labelStyles}>{title}</label>
         <input
           style={inputStyles}
-          placeholder={`Enter your ${title}`}
+          placeholder={placeholder ? placeholder : `Insira um ${title}`}
+          type={type || 'text'}
+          value={value}
           onChange={handleInputChange}
           onFocus={handleFocus}
           onBlur={handleBlur}
         />
       </div>
       <div className={'captionMsg'} style={captionStyles}>
-        <FontAwesomeIcon icon={faExclamationTriangle} />
-        <Separator horizontal size={4} />
-        <span>Invalid {title}</span>
+        {hasError && required && !value.trim() && <ErrorMessage message="Este campo é obrigatório" />}
+        {hasError && email && value && !validateEmail(value) && <ErrorMessage message="Seu Email é invalido" />}
+        {!hasError && passwordMismatchError && <ErrorMessage message="As senhas não coincidem" />}
       </div>
     </div>
   );
