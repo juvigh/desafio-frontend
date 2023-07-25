ea049d4679812226f6de2cd587b92d762c2bd429
Julio <juliolk.4321@gmail.com>
2023-07-25 18:06:22 -0300

    Modify H1 component to accept 'size' prop

--- src/styles/typography/h1.tsx
+++ src/styles/typography/h1.tsx
@@ -5,14 +5,15 @@ import { typography } from './typography';
 interface H1Props {
   children: React.ReactNode;
   color?: keyof typeof colors;
+  size?: keyof typeof typography.size;
 }
 
-export const H1 = ({ children, color }: H1Props) => {
+export const H1 = ({ children, color, size }: H1Props) => {
   return (
     <h1
       style={{
         fontFamily: typography.family.primary,
-        fontSize: typography.size.xxxxLarge,
+        fontSize: size ? typography.size[size] : 'xxxxLarge',
         fontWeight: typography.weight.semiBold,
         color: color ? colors[color] : colors.PrimaryDark,
         lineHeight: typography.lineHeight.large,
