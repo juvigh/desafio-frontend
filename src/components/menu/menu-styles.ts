5f49d60b303bf847a0ed8171ffd2acac6c5de271
Julio <juliolk.4321@gmail.com>
2023-07-28 13:50:12 -0300

    Update menu-styles.ts

--- src/components/menu/menu-styles.ts
+++ src/components/menu/menu-styles.ts
@@ -3,7 +3,7 @@ import { typography } from '../../styles/typography/typography';
 
 export const MenuContainer = {
   backgroundColor: `${colors.NeutralWhite}`,
-  boxShadow: `0 1.5px 0 solid ${colors.NeutralLight}`,
+  boxShadow: `0.5px 0px 0.5px 0 ${colors.NeutralLight}`,
   padding: '8px 0',
 };
 export const MenuItemStyles = {
